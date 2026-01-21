2e21c90dd7c26785445c47fd6b59f4e34377bce7
NwaeNandarAung97 <nandar.a@codetism.com>
2026-01-21 11:22:55 +0700

    implement StartGameScreen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import StartGameScreen from "@/screens/StartGameScreen";
+import {StyleSheet, ImageBackground} from "react-native";
+import {LinearGradient} from "expo-linear-gradient";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+      <ImageBackground
+        source={require('../assets/images/background.png')}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+          <StartGameScreen/>
+      </ImageBackground>
+    </LinearGradient>
   );
 }
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  }
+});
